bec98a273bcaf90bf9528744ab60368e9b5b9e07
ABBJamMon <54663152+ABBJamMon@users.noreply.github.com>
2022-04-29 10:19:16 +0200

    Discord bot test

--- MainBOT.py
+++ MainBOT.py
@@ -1,6 +1,12 @@
+from asyncio import tasks
+from enum import auto
+from discord.ext import tasks, commands
+from tracemalloc import start
 import discord
 import ctx
 import os
+
+from data import RSI
 
 client = discord.Client()
 
@@ -14,9 +20,18 @@ async def on_message(message):
         return
 
     if message.content.startswith('$test'):
-        await message.channel.send('Bot is functioning.')
+        await message.channel.send('Bot is functioning')
 
     elif message.content.startswith('$chart'):
         await message.channel.send(file=discord.File('C:\\Users\\S9jammon\\Documents\\GitHub\\DiscordBOT\\chart.png'))
 
-client.run('OTU2NTk0MjA5NjY0NjkyMjY0.YjyfyA.YgZ-S-6A8HBYNxW4qJphew3K0aA')
+@tasks.loop(hours = 1.0)
+async def auto_send():
+    channel = await client.fetch.channel('509390855703101452')
+    if RSI == [14]: 
+        channel.send('RSI under 14')
+
+    elif RSI == [30]:
+        channel.send('RSI over 30')
+
+client.run('OTU2NTk0MjA5NjY0NjkyMjY0.YjyfyA.iVl34IgRSAQxLrl73GrWfvAP-30')
